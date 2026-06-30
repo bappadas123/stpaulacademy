60a49857bb4e32697a15925ecdcd283719343dcd
bappadas123 <das_bappa2002@yahoo.co.in>
2026-06-30 14:00:40 +0530

    intital commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import ErrorPage from './pages/ErrorPage';
+
+function App() {
+  const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  }
+ 
+]);
+
+
+
+  return (
+    <>
+       <RouterProvider router={router} />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+   <>
+      <h> hello Home</h>
+    </>
+  )
+}
+
+export default Home
