427cd18a85234d9bda52c5b2e7508509c88026a7
bappadas123 <40697151+bappadas123@users.noreply.github.com>
2026-07-02 13:35:33 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import Home from './pages/Home';
